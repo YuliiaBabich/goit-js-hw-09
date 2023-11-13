147441242646c5f318d0f0961b90c1b27d17ffe8
YuliiaBabich <aim2002@ukr.net>
2023-11-13 15:24:25 +0200

    ywurvidlik

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,13 +4,79 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 
+const refs = {
+  startBtn: document.querySelector('button[data-action-start]'),
+  stopBtn: document.querySelector('button[data-action-stop]'),
+  calendar: document.querySelector('#datetime-picker'),
+};
+
+class Clockface {
+  initTime;
+  intervalId;
+  isActive = false;
+
+  constructor(render) {
+    this.render = render;
+  }
+  start() {
+    if (this.isActive) return;
+    this.isActive = true;
+    this.initTime = Date.now();
+    console.log('start');
+    this.intervalId = setInterval(() => {
+      const currentTime = Date.now();
+      const diff = currentTime - this.initTime;
+      const time = convertMsToTime(diff);
+      this.render(time);
+    }, 1000);
+  }
+  stop() {
+    if (!this.isActive) return;
+    console.log('stop');
+    this.isActive = false;
+    clearInterval(this.intervalId);
+  }
+}
+
+const clockface = new Clockface(render);
+
+refs.startBtn.addEventListener('click', () => {
+  clockface.start();
+});
+
+refs.stopBtn.addEventListener('click', () => {
+  clockface.stop();
+});
+
+function render(time) {
+  refs.clockface.textContent = time;
+}
+
+function convertMsToTime(milliseconds) {
+  let seconds = Math.floor(milliseconds / 1000);
+  let minutes = Math.floor(seconds / 60);
+  let hours = Math.floor(minutes / 60);
+
+  seconds = seconds % 60;
+  minutes = minutes % 60;
+  hours = hours % 24;
+
+  return `${padTo2Digits(hours)}:${padTo2Digits(minutes)}:${padTo2Digits(
+    seconds,
+  )}`;
+}
+
+function padTo2Digits(num) {
+  return num.toString().padStart(2, '0');
+}
+/*
 const TIMER_DELAY = 1000;
 let intervalId = null;
 let selectedDate = null;
 let currentDate = null;
 
-const calendar = document.querySelector('#datetime-picker');
-const startBtn = document.querySelector('[data-start-timer]');
+
+
 startBtn.disabled = true;
 
 Report.info(
@@ -106,4 +172,4 @@ const timer = {
   addLeadingZero(value) {
     return String(value).padStart(2, 0);
   },
-};
+};*/
